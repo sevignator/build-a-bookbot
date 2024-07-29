4eb7e4e07829d36260a26326c43a3eaa70872223
Mikaël Sévigny <sevignator@hotmail.com>
2024-07-29 10:15:57 -0400

    Add get_char_count() function

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+# Bookbot project
+
 My personal solution to the [Build a Bookbot](https://www.boot.dev/courses/build-bookbot) project from [Boot.dev](https://www.boot.dev/tracks/backend).
+
+## Set up
+
+1. From the root directory, create a directory called `books/`, and a new file within it called `frankenstein.txt`.
+2. Copy the contents of [*Frankenstein* by Mary Shelley](https://raw.githubusercontent.com/asweigart/codebreaker/master/frankenstein.txt) into `frankenstein.txt`.
+3. Run the main Python script.
+
+    ```python
+    python main.py
+    ```
--- main.py
+++ main.py
@@ -1,17 +1,40 @@
 def main():
-    book_path = "books/frankeinstein.txt"
+    """Main function for this program."""
+
+    book_path = "books/frankenstein.txt"
     text = get_file_text(book_path)
     word_count = get_word_count(text)
+    char_count = get_char_count(text)
     print(f"This document contains {word_count} words.")
+    print(char_count)
 
 
-def get_word_count(text):
+def get_word_count(text: str):
+    """Read a string and return a number indicating how many words it contains."""
+
     words = text.split()
     return len(words)
 
+def get_char_count(text: str):
+    """Read a string and return a dictionary indicating the count of each character."""
 
-def get_file_text(path_to_file):
-    with open(path_to_file) as f:
+    chars = {}
+
+    for char in text:
+        converted_char = char.lower()
+
+        if converted_char not in chars:
+            chars[converted_char] = 0
+
+        chars[converted_char] += 1
+
+    return chars
+
+
+def get_file_text(path_to_file: str):
+    """Open a text file from a given path and return its contents as a string."""
+
+    with open(path_to_file, encoding="utf-8") as f:
         return f.read()
 
 
