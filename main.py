e238ff1fe54c62abe6f580ffa1a17a8a123eaae2
Mikaël Sévigny <sevignator@hotmail.com>
2024-07-29 21:34:22 -0400

    Add print_report() function

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ def main():
     """Main function for this program."""
 
     book_path = "books/frankenstein.txt"
-    text = get_file_text(book_path)
-    word_count = get_word_count(text)
-    char_count = get_char_count(text)
-    print(f"This document contains {word_count} words.")
-    print(char_count)
+    print_report(book_path)
 
 
 def get_word_count(text: str):
@@ -14,6 +10,7 @@ def get_word_count(text: str):
 
     words = text.split()
     return len(words)
+
 
 def get_char_count(text: str):
     """Read a string and return a dictionary indicating the count of each character."""
@@ -36,6 +33,29 @@ def get_file_text(path_to_file: str):
 
     with open(path_to_file, encoding="utf-8") as f:
         return f.read()
+
+
+def print_report(path_to_file: str):
+    """Print a human-readable report to the console."""
+
+    text = get_file_text(path_to_file)
+    word_count = get_word_count(text)
+    char_count_dict = get_char_count(text)
+    char_count_list = list(
+        map(
+            lambda char: {"char": char, "count": char_count_dict[char]},
+            filter(lambda char: char.isalpha(), list(char_count_dict)),
+        )
+    )
+    char_count_list.sort(reverse=True, key=lambda item: item["count"])
+
+    print(f"--- Begin report of {path_to_file} ---")
+    print(f"{word_count} words found in the document\n")
+
+    for item in char_count_list:
+        print(f"The \'{item['char']}\' character was found {item['count']} times")
+
+    print("--- End report ---")
 
 
 main()
